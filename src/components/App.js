234fc2f834bc6814a7952b372b36cc2bc3cb52d3
dmb-2222 <dmb-2222@yandex.ru>
2019-06-08 17:14:23 +0300

    hw2

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Publications from '../publications.json'
+const App = () => (
+  <div>
+  <button onClick={evt => console.log(evt)}>Click me!</button>
+  </div>
+);
+
+export default App;
