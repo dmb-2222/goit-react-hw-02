a1b7c215f2df8e0be08c581048ae0e94ca7f40b3
dmb-2222 <dmb-2222@yandex.ru>
2019-06-26 20:35:59 +0300

    Updates

---
+++ precache-manifest.f997c9f6fca22e1d1f009a3296abcc44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6981b08bd72b1eb416e60854109e080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cacd647970235629c30",
+    "url": "/static/css/main.be89d028.chunk.css"
+  },
+  {
+    "revision": "5db2c5dea95c85b99740",
+    "url": "/static/js/2.a228a126.chunk.js"
+  },
+  {
+    "revision": "7cacd647970235629c30",
+    "url": "/static/js/main.f2b645f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
